623571eb26937630de0cfc7cb2b8d9b33144f836
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:38:52 +0000

    Validate uniqueness of users.phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :phone, :uniqueness => { :allow_blank => true }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
