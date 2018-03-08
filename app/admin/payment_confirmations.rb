248cace5f7c3871992a8dcffcb43ed9042d799f6
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:41:34 +0000

    Whitelist columns in PaymentConfirmation for ActiveAdmin

--- app/admin/payment_confirmations.rb
+++ app/admin/payment_confirmations.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register PaymentConfirmation do
+
+ permit_params :group_id, :date_posted, :image, :payment_method_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
