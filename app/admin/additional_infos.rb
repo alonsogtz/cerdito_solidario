cf924c400e3315aa6dea04cd669a7e4d1c07d1e8
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:42:04 +0000

    Whitelist columns in AdditionalInfo for ActiveAdmin

--- app/admin/additional_infos.rb
+++ app/admin/additional_infos.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register AdditionalInfo do
+
+ permit_params :description, :image, :user_id, :title
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
