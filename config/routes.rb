536f5c57812cd38db7582cebca426996ab7c47d7
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:41:00 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "personal_messages#index"
   # Routes for the Disbursement_option resource:
   # CREATE
