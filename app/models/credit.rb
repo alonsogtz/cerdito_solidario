92d91b31ac84ad0a1c12fc51533466a99f577b81
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:38:52 +0000

    Validate presence of credits.amount

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -5,6 +5,8 @@ class Credit < ApplicationRecord
 
   # Validations
 
+  validates :amount, :presence => true
+
   validates :amount, :numericality => { :greater_than_or_equal_to => 0 }
 
   validates :group_id, :presence => true
