c1c482f91f2311606e96d6ed505505e40d583ee3
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:41:40 +0000

    Whitelist columns in GroupComment for ActiveAdmin

--- app/admin/group_comments.rb
+++ app/admin/group_comments.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register GroupComment do
+
+ permit_params :user_id, :group_id, :body
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
