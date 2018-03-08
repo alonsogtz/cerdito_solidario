0e7d38534f75793b99edcb4804d70bf8ee849d6e
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:38:52 +0000

    Validate numericality of credits.amount

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -5,6 +5,8 @@ class Credit < ApplicationRecord
 
   # Validations
 
+  validates :amount, :numericality => { :greater_than_or_equal_to => 0 }
+
   validates :group_id, :presence => true
 
   validates :status, :presence => true
