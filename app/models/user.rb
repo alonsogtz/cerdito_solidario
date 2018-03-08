9b381ba5b8d19498099814d58eea74eaf625121c
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:38:52 +0000

    Validate presence of users.paternal_last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :paternal_last_name, :presence => true
+
   validates :phone, :uniqueness => { :allow_blank => true }
 
   # Include default devise modules. Others available are:
