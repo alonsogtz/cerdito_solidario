390dd9f282c45da92351d80d0b60dc8071b5810f
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:41:21 +0000

    Whitelist columns in Credit for ActiveAdmin

--- app/admin/credits.rb
+++ app/admin/credits.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Credit do
+
+ permit_params :user_id, :amount, :group_id, :pricing, :comments, :weekly_payment, :status, :disbursement_confirmation, :disbursement_confirmation_image, :disbursement_option_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
