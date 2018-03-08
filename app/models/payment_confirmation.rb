ee28d8385cfae23705c08bf4d1d72de53416cf8a
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:38:52 +0000

    Validate presence of payment_confirmations.group_id

--- app/models/payment_confirmation.rb
+++ app/models/payment_confirmation.rb
@@ -5,6 +5,8 @@ class PaymentConfirmation < ApplicationRecord
 
   # Validations
 
+  validates :group_id, :presence => true
+
   validates :image, :presence => true
 
   validates :payment_method_id, :presence => true
