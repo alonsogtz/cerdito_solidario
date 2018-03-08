718809b3cbc0b1e856e99e376905ef176b3c2552
Raghu Betina <raghu@firstdraft.com>
2018-03-08 21:42:51 +0000

    Only leader user can access show page

--- app/controllers/leaders_controller.rb
+++ app/controllers/leaders_controller.rb
@@ -1,5 +1,5 @@
 class LeadersController < ApplicationController
-  before_action :current_user_must_be_leader_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_leader_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_leader_user
     leader = Leader.find(params[:id])
